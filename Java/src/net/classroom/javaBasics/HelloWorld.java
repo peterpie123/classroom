70c9a03625cc96bfb6fda3607d7f6d7468f14af5
Peter Geertsema <peterjgeertsema@gmail.com>
2018-07-17 17:14:52 -0400

    Setup Java environment with IntelliJ

---
+++ Java/src/net/classroom/javaBasics/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.classroom.javaBasics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This is a single-line comment!
+        /* This is a
+          multiline comment! */
+
+        // The line below should not be commented -- uncomment it by removing the two slashes!
+        // System.out.println("Hello, World");
+    }
+}
---
+++ Java/src/net/classroom/javaBasics/PrimitiveVariables.java
@@ -0,0 +1,6 @@
+package net.classroom.javaBasics;
+
+public class PrimitiveVariables {
+    public static void main(String[] args) {
+    }
+}
