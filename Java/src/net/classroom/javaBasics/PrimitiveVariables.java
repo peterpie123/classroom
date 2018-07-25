f8ea4feab3d213781ef0afd7669b0784ec946249
Peter Geertsema <peterjgeertsema@gmail.com>
2018-07-25 15:35:36 -0400

    Work on Primitive Variables

--- Java/src/net/classroom/javaBasics/PrimitiveVariables.java
+++ Java/src/net/classroom/javaBasics/PrimitiveVariables.java
@@ -2,5 +2,14 @@ package net.classroom.javaBasics;
 
 public class PrimitiveVariables {
     public static void main(String[] args) {
+        //can be true or false
+        boolean hey = true;
+        //this line sets hey to false - pretty easy right!
+        hey = false;
+
+        //number, ranges from -2147483648 to 2147483647 It is a 32 bit signed integer
+        int integer = 17;
+
+
     }
 }
